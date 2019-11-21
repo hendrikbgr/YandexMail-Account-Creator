53fd21152a52b5878be71cc722d0045dcdebe416
hendrikbgr <hendrik@whalar.com>
2019-11-21 14:22:38 +0100

    Added Proxy from file choice
    Added the option to add own proxies to a text file which then get used to create accounts. The option to crawl accounts still exists.

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-# 🔥 Version 1.0 🔥
-
-We are excited to announce version 1.0 is here. It was an amazing journey the last couple of weeks setting this up and finally bringing out a stable version. Most processes will be automated and the time of creation will be faster than ever. With version 1.0 you will be able to loop the process of creation and create endless amounts of accounts whilst the script creates a table with all verified accounts so they will easy for you to use.  But I won’t stop with version 1.0, I have many plans for more features and some small tweaks are planned already. Also some new scripts will be added in future, for example I am planning to add an automatic login script so you can check you’re mails faster and automated.
-
 # ProtonMail Account Creator
 
 🚀 Create endless amounts of free ProtonMail Accounts. 🚀
+🏗 Manage your free ProtonMail Accounts. 🏗
 
-📌 Ver. 1.0.0 📌
+📌 Ver. 1.0.1 📌
 
 🤖 Full Proxy Support 🤖
 
 ## Features
 
 * Random inputs for Username & Password ✏️
-* Print out Account details after Creation 🧾
-* Works with ProxyCrawl for Full Proxy cover 🤫
+* Save Account details after Creation in .cvs file 🧾
+* Use CrawledProxies or use proxy file 🤫
 * Full Auto Account Creation 🤖
 
 ## Getting Started
@@ -27,6 +24,8 @@ These instructions will get you a copy of the project up and running on your loc
 Any problems with running the script and any questions please cantact me via Twitter [@hendrik_bgr](https://twitter.com/Hendrik_bgr)
 
 ### Prerequisites
+
+Download the latest version of the 'chromedriver' and place it in the 'driver' folder insde of the project folder.
 
 You need python 3 installed on your System.
 
@@ -44,20 +43,26 @@ pip install -r requirements.txt
 
 After that you can proceed to edit the Script.
 
-### Edit Script
-
-Open emails.py with any TextEditor.
-
-Change all needed values such as username, password and recovery email.
-
 ## Running 🏃🏽‍♂️
 
 To run this script open your Terminal in the project directory.
 
-To start the script enter:
+To start the Email Creator Script enter:
 
 ```
 python emails.py
+```
+
+To start the auto Login Script enter:
+
+```
+python login.py
+```
+
+To start the Password changer Script enter:
+
+```
+python newpassword.py
 ```
 
 ## Built With
@@ -68,7 +73,6 @@ python emails.py
 
 * **Hendrik Bgr** - *Initial work* - [HendrikBgr](https://github.com/hendrikbgr)
 
-
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
@@ -76,7 +80,6 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 ## Acknowledgments
 
 * Hat tip to anyone whose code was used
-* Inspiration
 
 ## To Do List 📝
 
@@ -85,11 +88,9 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 * Add more information to console when running ✅
 * Add ProxyScrape for Porxy Support ✅
 * Create requirements.txt file for easy installation ✅
-* Use 10 Minute Mail Api for verification and recovery email
+* Use 10 Minute Mail Api for verification and recovery email (paid service)
 
 ## Known Bugs 🐛
 
-* The crawled proxies don't always work. ✅
-* Proxy can be too slow and timeout script. ✅
 * Verification process still needs to be done manually. ⚠️
 
--- emails.py
+++ emails.py
@@ -14,10 +14,11 @@ import requests
 import csv
 import sys
 from proxyscrape import create_collector
+from selenium.webdriver.chrome.options import Options 
 import os
 clear = lambda: os.system('clear')
 clear()
-
+i = 0
 collector = create_collector('my-collector', 'https')
 
 print ('\033[31m' + """\
@@ -39,153 +40,194 @@ print ('\033[31m' + """\
 / /___/ /  /  __/ /_/ / /_/ /_/ / /    
 \____/_/   \___/\__,_/\__/\____/_/     
 """ + '\033[0m')
-print ('\033[31m' + "Auto Account Creator Script" + '\033[0m')
 
-restart = 2
-while (restart > 1):
+print ('\033[31m' + "Auto Account Creator Script" + '\033[0m')
+print('\033[31m' + "Pick a proxy option:" + '\033[0m')
+print('\033[31m' + "(1) - Crawl Free Proxies" + '\033[0m')
+print('\033[31m' + "(2) - Load from file" + '\033[0m')
+proxy_option = input('\033[31m' + "Enter you option number: " + '\033[0m')
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K") 
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K")
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K")
+sys.stdout.write("\033[F")
+sys.stdout.write("\033[K") 
+restart2 = 2
+while (restart2 > 1):
     # Pick an email for Verification. Replace 'YourEmail@Mail.com' with an email adress. (You can use 10min mail for this)
     verifymail = input('\033[31m' + "Enter Email Adress for Verification: " + '\033[0m')
 
     # Pick an email for Notification. Replace 'YourEmail@Mail.com' with an email adress. (You can use 10min mail for this)
-    notifymail = input('\033[31m' + "Enter Email Adress for Recovery: " + '\033[0m')                                                    
-    proxy_status = "false"
-    while (proxy_status == "false"):
+    notifymail = input('\033[31m' + "Enter Email Adress for Recovery: " + '\033[0m') 
 
-        # Retrieve only 'us' proxies
-        proxygrab = collector.get_proxy({'code': ('us', 'uk')})
-        proxy = ("{}:{}".format(proxygrab.host, proxygrab.port))
-        print ('\033[31m' + "Proxy:", proxy + '\033[0m')
-
-        try:
-            proxy_host = proxygrab.host
-            proxy_port = proxygrab.port
-            proxy_auth = ":"
-            proxies = {'http':'http://{}@{}:{}/'.format(proxy_auth, proxy_host, proxy_port)}
-            requests.get("http://example.org", proxies=proxies, timeout=1.5)
-
-        except OSError:
-            print ('\033[31m' + "Proxy Connection error!" + '\033[0m')
-            time.sleep(1)
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K") 
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K") 
-            proxy_status = "false"
-        else:
-            print ('\033[31m' + "Proxy is working..." + '\033[0m')
-            time.sleep(1)
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K") 
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K") 
-            proxy_status = "true"
-
-
-    else:
-        from selenium.webdriver.chrome.options import Options 
-
-        warnings.filterwarnings("ignore", category=DeprecationWarning) 
-
-        options = Options()
-        options.add_argument('--proxy-server={}'.format(proxy))
-
-        # Change Path to Chrome Driver Path (or move your ChromeDriver into the project folder)
-        driver = webdriver.Chrome(executable_path='/driver/chromedriver', chrome_options=options)
-
-        url = 'http://protonmail.com/signup'
-
-        def randomStringDigits(stringLength=13):
-            # Generate a random string of letters and digits
-            lettersAndDigits = string.ascii_letters + string.digits
-            return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
-
-        rngusername = randomStringDigits(13)
-        rngpassword = randomStringDigits(15)
-
-        driver.get(url)
-
-        time.sleep(4)
-
-        driver.find_element_by_class_name('panel-heading').click()
-
-        time.sleep(1)
-
-        driver.find_element_by_id('freePlan').click()
-
-        time.sleep(4)
-
-        driver.switch_to_frame(0)
-
-        time.sleep(3)
-
-        driver.find_element_by_id('username').send_keys(rngusername)
-
-        time.sleep(1)
-
-        driver.switch_to.default_content()
-
-        time.sleep(1)
-
-        driver.find_element_by_id('password').send_keys(rngpassword)
-
-        time.sleep(1)
-
-        driver.find_element_by_id('passwordc').send_keys(rngpassword)
-
-        time.sleep(1)
-
-        driver.switch_to_frame(1)
-
-        time.sleep(1)
-
-        driver.find_element_by_id('notificationEmail').send_keys(notifymail)
-
-        time.sleep(1)
-
-        driver.find_element_by_name('submitBtn').click()
-
-        time.sleep(6)
-
-        driver.find_element_by_id('id-signup-radio-email').click()
-
-        time.sleep(1)
-
-        driver.find_element_by_id('emailVerification').send_keys(verifymail)
-
-        time.sleep(1)
-
-        driver.find_element_by_class_name('codeVerificator-btn-send').click()
-
-        time.sleep(3)
-
-        print ('\033[31m' + "Your New Email Adress is: ", rngusername,"@protonmail.com", sep='' + '\033[0m')
-        print ('\033[31m' + "Your New Email Password is: "  + '\033[0m' , rngpassword)
-
-        complete = "false"
-
-        while (complete == "false"):
-            complete_q = input("Did you complete the Verification process? y/n: ")
-
-            if complete_q == "y":
-                driver.close()
-                csvData = [[rngusername + '@protonmail.com', rngpassword]]
-                with open('list.csv', 'a') as csvFile:
-                    writer = csv.writer(csvFile)
-                    writer.writerows(csvData)
-                csvFile.close()
-                print ('Great! We added you account details to the table.')
-                complete = "true"
-            
+    if proxy_option == "2":
+        print('\033[31m' + "Getting Proxies from file..." + '\033[0m')
+        with open('./proxy.txt', 'r') as data:
+            proxy_lines = [line.strip() for line in data]
+        proxy_from_file = "false"
+        while (proxy_from_file == "false"):
+            i += 1
+            print('\033[31m' + proxy_lines[i] + '\033[0m')
+            try:
+                proxies_file = {'http':'http://:@{}/'.format(proxy_lines[i])}
+                requests.get("http://protonmail.com/", proxies=proxies_file, timeout=1.5)
+            except OSError:
+                print ('\033[31m' + "Proxy Connection error!" + '\033[0m')
+                proxy_from_file = "false"
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
             else:
-                print ('Please try verifing and try again')
+                print ('\033[31m' + "Proxy is working..." + '\033[0m')
+                i += 1
+                options = Options()
+                options.add_argument('--proxy-server={}'.format(proxy_lines[i]))
+                proxy_from_file = "true"
+                proxy_option 
+                
+    else:
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K") 
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")  
+        proxy_status = "false"
+        while (proxy_status == "false"):
+
+            # Retrieve only 'us' proxies
+            proxygrab = collector.get_proxy({'code': ('us')})
+            proxy = ("{}:{}".format(proxygrab.host, proxygrab.port))
+            print ('\033[31m' + "Proxy:", proxy + '\033[0m')
+
+            try:
+                proxy_host = proxygrab.host
+                proxy_port = proxygrab.port
+                proxy_auth = ":"
+                proxies = {'http':'http://{}@{}:{}/'.format(proxy_auth, proxy_host, proxy_port)}
+                requests.get("http://protonmail.com/", proxies=proxies, timeout=1.5)
+
+            except OSError:
+                print ('\033[31m' + "Proxy Connection error!" + '\033[0m')
                 time.sleep(1)
-                complete = "false"
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
+                proxy_status = "false"
+            else:
+                print ('\033[31m' + "Proxy is working..." + '\033[0m')
+                time.sleep(1)
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
+                sys.stdout.write("\033[F")
+                sys.stdout.write("\033[K") 
+                proxy_status = "true"
+                options = Options()
+                options.add_argument('--proxy-server={}'.format(proxy))
+
+
+    warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+    # Change Path to Chrome Driver Path (or move your ChromeDriver into the project folder)
+    driver = webdriver.Chrome(executable_path='./driver/chromedriver', chrome_options=options)
+
+    url = 'http://protonmail.com/signup'
+
+    def randomStringDigits(stringLength=13):
+        # Generate a random string of letters and digits
+        lettersAndDigits = string.ascii_letters + string.digits
+        return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+    rngusername = randomStringDigits(13)
+    rngpassword = randomStringDigits(15) 
+    
+    driver.get(url)
+
+    time.sleep(4)
+
+    driver.find_element_by_class_name('panel-heading').click()
+
+    time.sleep(1)
+
+    driver.find_element_by_id('freePlan').click()
+
+    time.sleep(4)
+
+    driver.switch_to_frame(0)
+
+    time.sleep(3)
+
+    driver.find_element_by_id('username').send_keys(rngusername)
+
+    time.sleep(1)
+
+    driver.switch_to.default_content()
+
+    time.sleep(1)
+
+    driver.find_element_by_id('password').send_keys(rngpassword)
+
+    time.sleep(1)
+
+    driver.find_element_by_id('passwordc').send_keys(rngpassword)
+
+    time.sleep(1)
+
+    driver.switch_to_frame(1)
+
+    time.sleep(1)
+
+    driver.find_element_by_id('notificationEmail').send_keys(notifymail)
+
+    time.sleep(1)
+
+    driver.find_element_by_name('submitBtn').click()
+
+    time.sleep(6)
+
+    driver.find_element_by_id('id-signup-radio-email').click()
+
+    time.sleep(1)
+
+    driver.find_element_by_id('emailVerification').send_keys(verifymail)
+
+    time.sleep(1)
+
+    driver.find_element_by_class_name('codeVerificator-btn-send').click()
+
+    time.sleep(3)
+
+    print ('\033[31m' + "Your New Email Adress is: ", rngusername,"@protonmail.com", sep='' + '\033[0m')
+    print ('\033[31m' + "Your New Email Password is: "  + '\033[0m' , rngpassword)
+    
+    complete = "false"
+
+    while (complete == "false"):
+        complete_q = input('\033[31m' + "Did you complete the Verification process? y/n: " + '\033[0m')
+
+        if complete_q == "y":
+            driver.close()
+            csvData = [[rngusername + '@protonmail.com', rngpassword]]
+            with open('list.csv', 'a') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerows(csvData)
+            csvFile.close()
+            print ('\033[31m' + 'Great! We added you account details to the table.' + '\033[0m')
+            complete = "true"
+        
         else:
-            restart_s = input("Do you want to restart the Script and create more Accounts? y/n: ")
-            if restart_s == "y":
-                restart ++ 1
-                clear()
-                print ('\033[31m' + """\
+            print ('\033[31m' + 'Please try verifing and try again' + '\033[0m')
+            time.sleep(1)
+            complete = "false"
+    else:
+        restart_s = input('\033[31m' + "Do you want to restart the Script and create more Accounts? y/n: " + '\033[0m')
+        if restart_s == "y":
+            i += 1
+            clear()
+            print ('\033[31m' + """\
     ____             __              __  ___      _ __
    / __ \_________  / /_____  ____  /  |/  /___ _(_) /
   / /_/ / ___/ __ \/ __/ __ \/ __ \/ /|_/ / __ `/ / / 
@@ -204,10 +246,9 @@ while (restart > 1):
 / /___/ /  /  __/ /_/ / /_/ /_/ / /    
 \____/_/   \___/\__,_/\__/\____/_/     
 """ + '\033[0m')
+            restart2 == 3
 
-            else:
-                print ("Ok! The script is exiting now.")
-                time.sleep(1)
-                exit()
-else:
-    print("something")
+        else:
+            print ('\033[31m' + "Ok! The script is exiting now." + '\033[0m')
+            time.sleep(1)
+            exit()
